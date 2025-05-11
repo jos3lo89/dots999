9b87702d14280e27266940953618c822771b60a4
jos3lo89 <ganbaru743@gmail.com>
2025-05-11 14:15:30 -0500

    remove allnvim

--- allnvim/nvim/lua/config/keymaps.lua
+++
@@ -1,9 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-
--- Map Ctrl+b in insert mode to delete to the end of the word without leaving insert mode
-vim.keymap.set("i", "<C-b>", "<C-o>de")
-
--- Map Ctrl+c to escape from other modes
-vim.keymap.set({ "i", "n", "v" }, "<C-c>", [[<C-\><C-n>]])
